74ace10f04124cf7007c821fc6906d5f564db4d0
lukma99 <lukm@mailbox.org>
2023-03-09 09:21:11 +0100

    add a helm repo update

--- scripts/setup_cluster.sh
+++ scripts/setup_cluster.sh
@@ -58,6 +58,7 @@ kubectl create namespace argocd
 
 echo "Installing Argo CD"
 helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
 helm upgrade --install \
   --namespace argocd \
   --version ${ARGO_HELM_CHART_VERSION} \
