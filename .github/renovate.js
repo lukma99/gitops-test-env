b3ec26c6264df4dea9287461331bf91eefd40935
lukma99 <lukm@mailbox.org>
2023-03-13 15:22:07 +0100

    remove onboarding

---
+++ .github/renovate.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+    "extends": [
+        "config:base"
+    ],
+    "platform": "github",
+    "username": "renovate-bot",
+    "gitAuthor": "Renovate Bot <renovate@users.noreply.github.com>",
+    "repositories": [
+        "lukma99/gitops-test-env"
+    ],
+
+    "enabledManagers": ["kustomize"],
+    "kustomize": {
+        "ignorePaths": [
+            "**/01-manual/**",
+            "**/02-ci-pipeline/**",
+            "**/03-image-updater/**",
+            "**/05-preview-environments/**"
+        ]
+    }
+};
